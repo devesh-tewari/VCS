6b6ea6ffe115a3728191883cedffcae033eec1cf
Devesh <devesh.tewari@students.iiit.ac.in>
2018-11-04 20:06:03 +0530

    vcs add in progress

--- objects.h
+++ objects.h
@@ -6,12 +6,23 @@
 #include <set>
 using namespace std;
 
+/* Type and permissions format:
+   16 low bits: 4-bit object type, 3-bit unused, 9-bit unix permissions.
+   For blob, 1st type bit is set, for tree, second one and for commit,
+   third one */
+
 class Blob
 {
   string name;
   int type_and_permissions;
   string sha1;
   string bin_data;
+
+  Tree(int Name, int Type_and_permissions)
+  {
+    name = Name;
+    type_and_permissions = Type_and_permissions;
+  }
 }
 
 class Tree
@@ -19,8 +30,13 @@ class Tree
   string name;
   int type_and_permissions;
   string sha1;
-  set<string> blobs_sha1;
-  set<string> trees_sha1;
+  set<string> sha1_pointers;
+
+  Tree(int Name, int Type_and_permissions)
+  {
+    name = Name;
+    type_and_permissions = Type_and_permissions;
+  }
 }
 
 class Commit
@@ -33,13 +49,15 @@ class Commit
   string message;
 }
 
-void save_blob(Blob);
+void set_time_and_permissions();
+
+/*void save_blob(Blob);
 void load_blob(Blob);
 
 void save_tree(Tree);
 void load_tree(Tree);
 
 void save_commit(Commit);
-void load_commit(Commit);
+void load_commit(Commit);*/
 
 #endif OBJECTS_H
